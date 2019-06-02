181d7982801a9c5914a44989515bd4a3706319fc
Yitong <zyt123456789@gmail.com>
2019-06-02 18:26:16 +0800

    Update deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,8 +2,12 @@
 
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
+    rm _site/travis-ci-deploy
+    rm _site/travis-ci-deploy.enc
+    echo "==== copy file ===="
+    rsync -r --delete-after --quiet _site/ wqjrzolc@treebeard.node.idc.wiki:/home/wqjrzolc/gdgxian.org/
+    
     ssh wqjrzolc@treebeard.node.idc.wiki <<EOF
-    rm -rf /home/wqjrzolc/gdgxian.org/*
     touch /home/wqjrzolc/gdgxian.org/.htaccess
     echo "
       <IfModule mod_rewrite.c>
@@ -11,9 +15,6 @@ then
             RewriteCond %{HTTPS} off
             RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
       </IfModule>
-      " > .htaccess
+      " > /home/wqjrzolc/gdgxian.org/.htaccess
 EOF
-
-    echo "==== copy file ===="
-    rsync -r --delete-after --quiet _site/ wqjrzolc@treebeard.node.idc.wiki:/home/wqjrzolc/gdgxian.org/
 fi
