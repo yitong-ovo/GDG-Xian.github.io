8ce2289b1d762fb7ab5da3c1828cf540ecc6e115
Yitong <zyt123456789@gmail.com>
2019-06-02 14:53:46 +0800

    Create deploy.sh

---
+++ _scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+then
+    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
+    rm -rf site
+    mkdir site
+    quit
+EOF
+
+    cd _site || exit
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" site .
+fi
