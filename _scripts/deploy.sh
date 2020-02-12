bf612bdec107fbb061685a0fb90d2285150df82d
Yitong <github@enl.sx>
2020-02-12 13:46:50 +0800

    Update deploy.sh
    尝试解决 scp 拷贝整个网站缓慢的问题

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -4,10 +4,14 @@ if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     rm _site/travis-ci-deploy
     rm _site/travis-ci-deploy.enc
+    echo "==== compression files ===="
+    zip -r webroot.zip _site/*
+    echo "==== file size: $(du -h webroot.zip) ===="
     echo "==== copy file ===="
-    scp -r _site/* foxtjryj@202.95.9.110:/httpdocs/gdgxian.org/
+    scp webroot.zip foxtjryj@202.95.9.110:/
     
     ssh foxtjryj@202.95.9.110 <<EOF
+    unzip -o webroot.zip -d /httpdocs/gdgxian.org/
     touch /httpdocs/gdgxian.org/.htaccess
     echo "
       <IfModule mod_rewrite.c>
@@ -16,5 +20,5 @@ then
             RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]
       </IfModule>
       " > /httpdocs/gdgxian.org/.htaccess
-EOF
+    EOF
 fi
